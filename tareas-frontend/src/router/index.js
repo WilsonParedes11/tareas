715c3408084b9bb1106718ea414a57715a22e3cc
WilsonParedes11 <wilsonparedes87@gmail.com>
2025-05-07 20:29:38 -0500

    feat: Configuracion de las rutas para el consumo del api

--- tareas-frontend/src/router/index.js
+++ tareas-frontend/src/router/index.js
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import TasksView from '../views/TasksView.vue'
+import TaskDetailView from '../views/TaskDetailView.vue'
+import CreateTaskView from '../views/CreateTaskView.vue'
+import EditTaskView from '../views/EditTaskView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,14 +16,57 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      meta: { requiresGuest: true },
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+      meta: { requiresGuest: true },
+    },
+    {
+      path: '/tasks',
+      name: 'tasks',
+      component: TasksView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/tasks/:id',
+      name: 'task-detail',
+      component: TaskDetailView,
+      props: true,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/tasks/create',
+      name: 'create-task',
+      component: CreateTaskView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/tasks/:id/edit',
+      name: 'edit-task',
+      component: EditTaskView,
+      props: true,
+      meta: { requiresAuth: true },
     },
   ],
+})
+
+// Navegación guard para proteger rutas
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = !!localStorage.getItem('token')
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next({ name: 'login' })
+  } else if (to.meta.requiresGuest && isAuthenticated) {
+    next({ name: 'tasks' })
+  } else {
+    next()
+  }
 })
 
 export default router
